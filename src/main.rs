7833dba14d940b6ec756a2af5c1201e4ca37195d
Markus Gasser <markus.gasser@mailbox.org>
2023-08-08 19:40:55 +0000

    Restart using axum, askama

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-#[macro_use] extern crate rocket;
+use std::net::SocketAddr;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use askama::Template;
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root));
+
+    // run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+// basic handler that responds with a static string
+async fn root() -> HelloTemplate<'static> {
+    HelloTemplate { name: "Dolly" }
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
 }
